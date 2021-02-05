33d194787ed795e8992d463ac993ceb360aa57df
Kirill Leon <59933683+KirillLeon@users.noreply.github.com>
2021-02-05 17:49:03 +0300

    Add files via upload

--- main_window.py
+++ main_window.py
@@ -3,12 +3,21 @@ from consts import *
 
 class MainWindow:
     def __init__(self, x: int, y: int, z: int):
+        self.bg = pygame.image.load('data/map.png')
+        self.size = self.bg.get_size()
+
+        pygame.init()
+        pygame.display.set_caption('Yandex Map')
+        self.screen = pygame.display.set_mode(self.size)
+
         self.x = x
         self.y = y
         self.z = z
 
         self.get_image()
+        self.running = False
         self.mainloop()
+
 
     def get_image(self):
         import requests
@@ -27,15 +36,20 @@ class MainWindow:
         map_file = "data/map.png"
         with open(map_file, "wb") as file:
             file.write(response.content)
+        self.bg = pygame.image.load('data/map.png')
+        self.size = self.bg.get_size()
 
     def mainloop(self):
-        pygame.init()
-        bg = pygame.image.load("data/map.png")
-        size = bg.get_size()
-        pygame.display.set_caption('Yandex Map')
-        screen = pygame.display.set_mode(size)
-        screen.blit(bg, [0, 0])
-        pygame.display.flip()
-        while pygame.event.wait().type != pygame.QUIT:
-            screen.blit(bg, [0, 0])
-        pygame.quit()
+        self.running = True
+
+        while self.running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit(0)
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_PAGEDOWN:
+                        print("Hhhhhh")
+                    elif event.key == pygame.K_PAGEUP:
+                        print("sggrgrr")
+            self.screen.blit(self.bg, [0,0])
+            pygame.display.flip()
