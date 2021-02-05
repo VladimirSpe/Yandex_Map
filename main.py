f8e938a6b97249a0fbe6c1e4175537ab3adcad2e
JktuJQ <74969604+JktuJQ@users.noreply.github.com>
2021-02-05 16:52:46 +0300

    Created main_window file
    YandexLyceum exercise #1

--- main.py
+++ main.py
@@ -1,48 +1,18 @@
-def scre_pyg():
-    import pygame
-    pygame.init()
-    getImage()
-    bg = pygame.image.load("map.png")
-    size = bg.get_size()
-    pygame.display.set_caption('Yandex_Map_ex1')
-    screen = pygame.display.set_mode(size)
-    screen.blit(bg, [0, 0])
-    pygame.display.flip()
-    while pygame.event.wait().type != pygame.QUIT:
-        screen.blit(bg, [0, 0])
-    pygame.quit()
+from consts import *
 
 
-def getImage():
-    import requests
-    import sys
-    size = cord_inp()
-    response = 0
-    if size != 0:
-        map_request = f"""http://static-maps.yandex.ru/1.x/?ll={size[1]},{size[0]}&spn={size[2]},{size[2]}&l=map"""
-        response = requests.get(map_request)
-    else:
-        map_request = 'Неверные координаты'
-    if not response:
-        print("Ошибка выполнения запроса:")
-        print(map_request)
-        print("Http статус:", response.status_code, "(", response.reason, ")")
-        sys.exit(1)
-    map_file = "map.png"
-    with open(map_file, "wb") as file:
-        file.write(response.content)
-
-
-def cord_inp():
-    import sys
+def main(argc: int, argv: typing.List[str]):
     try:
-        x = int(sys.argv[1])
-        y = int(sys.argv[2])
-        z = int(sys.argv[3])
+        x = int(argv[1])
+        y = int(argv[2])
+        z = int(argv[3])
     except Exception:
-        return 0
-    return str(x), str(y), str(z)
+        print("Некорректный ввод")
+        sys.exit(-1)
+
+    from main_window import MainWindow
+    MainWindow(x, y, z)
 
 
 if __name__ == '__main__':
-    scre_pyg()
+    main(len(sys.argv), sys.argv)
