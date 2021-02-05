b0129e1f072d23707725a94ff1da71f2f980a09b
VladimirSpe <75093117+VladimirSpe@users.noreply.github.com>
2021-02-05 16:32:46 +0300

    First Form

--- main.py
+++ main.py
@@ -1,11 +1,48 @@
-from consts import *
+def scre_pyg():
+    import pygame
+    pygame.init()
+    getImage()
+    bg = pygame.image.load("map.png")
+    size = bg.get_size()
+    pygame.display.set_caption('Yandex_Map_ex1')
+    screen = pygame.display.set_mode(size)
+    screen.blit(bg, [0, 0])
+    pygame.display.flip()
+    while pygame.event.wait().type != pygame.QUIT:
+        screen.blit(bg, [0, 0])
+    pygame.quit()
 
-import sys
+
+def getImage():
+    import requests
+    import sys
+    size = cord_inp()
+    response = 0
+    if size != 0:
+        map_request = f"""http://static-maps.yandex.ru/1.x/?ll={size[1]},{size[0]}&spn={size[2]},{size[2]}&l=map"""
+        response = requests.get(map_request)
+    else:
+        map_request = 'Неверные координаты'
+    if not response:
+        print("Ошибка выполнения запроса:")
+        print(map_request)
+        print("Http статус:", response.status_code, "(", response.reason, ")")
+        sys.exit(1)
+    map_file = "map.png"
+    with open(map_file, "wb") as file:
+        file.write(response.content)
 
 
-def main(argc: int, argv: typing.List[str]):
-    return 0
+def cord_inp():
+    import sys
+    try:
+        x = int(sys.argv[1])
+        y = int(sys.argv[2])
+        z = int(sys.argv[3])
+    except Exception:
+        return 0
+    return str(x), str(y), str(z)
 
 
 if __name__ == '__main__':
-    main(len(sys.argv), sys.argv)
+    scre_pyg()
